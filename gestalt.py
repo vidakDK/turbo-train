aca84b8579c393a3bb7c7a9cf14b038c7aa649b0
unknown <vidakdk@gmail.com>
2018-09-27 17:29:23 +0200

    further histogram manipulation for k metric

--- gestalt.py
+++ gestalt.py
@@ -2,17 +2,23 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 from math import sqrt
-from scipy.optimize import brute
+from scipy.optimize import brute, minimize_scalar
 
 # img = cv2.imread("9.jpg")
+
+
 
 class ColorExtractor:
 
     def __init__(self, img_path):
         self.img = cv2.imread(img_path)
         self.luv = luv = cv2.cvtColor(self.img, cv2.COLOR_BGR2Luv)
+        print(luv.min, luv.max)
         self.hist_lightness = cv2.calcHist([luv], [0], None, [256], [0, 256])
-        plt.plot(self.hist_lightness)
+        self.hist_u = cv2.calcHist([luv], [1], None, [256], [0, 256])
+        self.hist_v = cv2.calcHist([luv], [2], None, [256], [0, 256])
+
+        plt.plot(self.hist_lightness, label='lightness')
         plt.show()
 
     def k(self, t):
@@ -24,36 +30,49 @@ class ColorExtractor:
         """
         d0 = np.std(self.hist_lightness)**2
         n0 = np.sum(self.hist_lightness)
-        hist_higher = self.hist_lightness.copy()
-        hist_higher[:t] = 0
-        hist_lower = self.hist_lightness.copy()
-        hist_lower[t:] = 0
+        hist_higher = self.hist_lightness[t:]
+        hist_lower = self.hist_lightness[:t]
         d1 = np.std(hist_lower)
         n1 = np.sum(hist_lower)
         d2 = np.std(hist_higher)
         n2 = np.sum(hist_higher)
-        plt.plot(hist_higher, label=f"higher, t={t}")
-        plt.plot(hist_lower, label=f"higher, t={t}")
+        # plt.plot(hist_higher, label=f"higher, t={t}")
+        # plt.plot(hist_lower, label=f"higher, t={t}")
         return (sqrt(d1*n1) + sqrt(d2*n2)) / (sqrt(d0*n0))
 
     def min_k(self):
-        r = (slice(1, 255, 1), )
-        x0, fval, grid, jout = brute(self.k, r, full_output=True)
-        print(x0, fval)
+        lst = []
+        for t in range(1, 255):
+            lst.append((t, self.k(t)))
+        ts, ks = zip(*lst)
+        plt.plot(ks, label='approach 1')
+        plt.legend()
+        plt.show()
+        return min(lst, key=lambda x: x[1])
 
 # Get brightness histogram
 # hist = cv2.calcHist([luv], [0], None, [256], [0, 256])
 # hist,bins = np.histogram(img.ravel(),256,[0,256])
 
 
+
+
 if __name__ == "__main__":
-    c = ColorExtractor("9.jpg")
+    c = ColorExtractor("cloud.png")
     # print(c.k(64), c.k(128), c.k(192))
     # print(c.k(64))
     # plt.legend()
     # plt.show()
-    c.min_k()
-
+    t, k = c.min_k()
+    print(t,k)
+    first_cluster_mask = cv2.inRange(c.luv, np.array([0,0,0]), np.array([t,255,255]))
+    second_cluster_mask = cv2.inRange(c.luv, np.array([t,0,0]), np.array([255,255,255]))
+    first_cluster = cv2.bitwise_and(c.img, c.img, mask=first_cluster_mask)
+    second_cluster = cv2.bitwise_and(c.img, c.img, mask=second_cluster_mask)
+    cv2.imshow('first_cluster', cv2.resize(first_cluster, (960, 540)))
+    cv2.imshow('second_cluster', cv2.resize(second_cluster, (960, 540)))
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
 # # get only blue:
 # lower_blue = np.array([110,50,50])
 # upper_blue = np.array([130,255,255])
