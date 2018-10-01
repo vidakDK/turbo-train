891e1d44dea46e7573b0d6b6b1a7bdaf3d60f5f5
Vidak Kazic <vidakdk@gmail.com>
2018-10-01 11:39:29 +0200

    added colors module with k-means color extraction

--- colors.py
+++ colors.py
@@ -1,9 +1,84 @@
 import cv2
 import numpy as np
 from matplotlib import pyplot as plt
+from sklearn.cluster import KMeans
 
 
 class Colors:
-    def __init__(self, img_path):
+    def __init__(self, img_path, c):
         self.img = cv2.imread(img_path)
-        self.luv = luv = cv2.cvtColor(self.img, cv2.COLOR_BGR2Luv)
+        self.img_rgb = cv2.cvtColor(self.img, cv2.COLOR_BGR2RGB)
+        self.img_luv = cv2.cvtColor(self.img, cv2.COLOR_BGR2Luv)
+        self.c = c
+        if c == "rgb":
+            self.img_converted = self.img_rgb
+        else:
+            self.img_converted = self.img_luv
+
+    def _find_clusters(self):
+        image = self.img_converted.reshape((self.img_converted.shape[0] * self.img_converted.shape[1], 3))
+
+        n_clusters = 4
+        self.clusters = KMeans(n_clusters=n_clusters)
+        self.clusters.fit(image)
+
+    def _centroid_histogram(self):
+        # grab the number of different clusters and create a histogram
+        # based on the number of pixels assigned to each cluster
+        n_labels = np.arange(0, len(np.unique(self.clusters.labels_)) + 1)
+        self.hist, _ = np.histogram(self.clusters.labels_, bins=n_labels)
+
+        # normalize the histogram, such that it sums to one
+        self.hist = self.hist.astype("float")
+        self.hist /= self.hist.sum()
+
+    def _plot_colors(self):
+        # initialize the bar chart representing the relative frequency
+        # of each of the colors
+        bar = np.zeros((50, 300, 3), dtype="uint8")
+        start_x = 0
+
+        # loop over the percentage of each cluster and the color of
+        # each cluster
+        for percent, color in zip(self.hist, self.clusters.cluster_centers_):
+            # plot the relative percentage of each cluster
+            end_x = start_x + (percent * 300)
+            cv2.rectangle(
+                bar, (int(start_x), 0), (int(end_x), 50),
+                color.astype("uint8").tolist(), -1
+            )
+            start_x = end_x
+
+        # return the bar chart
+        return bar
+
+    def find_colors(self):
+        # Plot original image
+        plt.figure()
+        plt.imshow(self.img_rgb)
+
+        self._find_clusters()
+        self._centroid_histogram()
+        bar = self._plot_colors()
+
+        # Plot color bar:
+        if self.c == "luv":
+            bar = cv2.cvtColor(bar, cv2.COLOR_LUV2RGB)
+        plt.figure()
+        plt.axis("off")
+        plt.imshow(bar)
+
+
+
+if __name__ == "__main__":
+    i1 = "images/9small.jpg"
+    i2 = "images/15small.jpg"
+    i3 = "images/small.jpg"
+    i4 = "images/7small.jpg"
+
+
+    c = Colors(i4, "luv")
+    c.find_colors()
+    c = Colors(i4, "rgb")
+    c.find_colors()
+    plt.show()
